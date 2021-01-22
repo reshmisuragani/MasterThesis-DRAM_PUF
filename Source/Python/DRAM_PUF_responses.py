19df1c8b349fbddbae8ab4dded106f835fc7cdfe
reshmisuragani <reshmisuragani@gmail.com>
2021-01-22 08:54:39 +0100

    removed unwanted files

--- Source/Python/DRAM_PUF_responses.py
+++ Source/Python/DRAM_PUF_responses.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[6]:
 
 
 import os
 cwd = os.getcwd()
-f = open(r"C:\Users\reshm\dpuf\G02_25control\G02_1_1_28800sec.bin", "rb")
+f = open(r"output1.txt", "rb")
 num = list(f.read())
 b = ''.join('{:08b}'.format(c) for c in num)
 print(num)
+#for x in num:
+#    print(hex(x))
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
